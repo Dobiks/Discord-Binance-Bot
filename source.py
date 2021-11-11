e616fa6965d0f46855260c68b8705255211fee3b
Dobiks <49527911+Dobiks@users.noreply.github.com>
2021-11-11 18:18:25 +0100

    .

--- source.py
+++ source.py
@@ -21,46 +21,37 @@ def receive_json_response(ws):
 def binance(content):
     content = content.split()
     print(content[0])
-    if content[0] == "@ABC": #Check if message starts with role mention
-        symbol = content[1].replace('#', '').replace('/', '')
-        buy_prices = content[7].split('-')
-        sell_prices = content[12].split('-')
+    if content[0] == "Now" and content[1] == "tracking":
+        symbol = content[2]
+        #buy_prices = 0#content[7].split('-')
+        sell_prices = [content[6:10]]
+        sell_prices=sell_prices[0]
+        for a in range(len(sell_prices)):
+            sell_prices[a]=sell_prices[a].replace(",","")
         stop_price = content[-1]
-        print(symbol, buy_prices, sell_prices, stop_price)
-        return symbol, buy_prices, sell_prices, stop_price
-    else:
-        return 0
+        # print(wolumen, sell_prices, stop_price)
+        return symbol, sell_prices, stop_price
 
 
 class Signal:
-    def __init__(self, symbol, buy_prices, sell_prices, stop_price):
+    def __init__(self, symbol, sell_prices, stop_price):
         self.symbol = symbol
-        self.min_price = client.get_symbol_info(symbol=self.symbol)['filters'][0][
-            'minPrice']  # format(float(client.get_symbol_info(symbol=self.symbol)['filters'][0]['minPrice']), '.9f')
-        self.buy_prices = buy_prices
+        self.min_price = client.get_symbol_info(symbol=self.symbol)['filters'][0]['minPrice']
         self.sell_prices = sell_prices
         self.stop_price = stop_price
-        self.price = client.get_avg_price(symbol=self.symbol)['price']
-        self.avg_price = self.to_satoshi(self.price)  # self.price.replace('0', '').replace('.', '')
-        self.precision = self.calc_precision()
-        self.buy_quantity = format(0.01 / float(self.avg_price) * 100000000, str(self.precision).join('.f'))
-        self.sell_quantity = self.calc_quantity()
         self.stop_limit = self.calc_stop_limit()
-        self.adjusted_stop = self.adjust_price(stop_price)
-
-    def to_satoshi(self, price):
-        price = price.replace('.', '')
-        price = list(price)
-        idx = []
-        while 1:
-            if price[0] == '0':
-                price.pop(0)
-            else:
-                break
-
-        tmp = "".join(price)
-        print("Current price in satoshi: ", tmp)
-        return tmp
+        self.price = client.get_avg_price(symbol=self.symbol)['price']
+        self.precision = self.calc_precision()
+        self.buy_quantity = format(float(0.0077/float(self.price)), str(self.precision).join('.f'))
+        self.sell_quantity = self.calc_sell_quantity()
+        self.ok = 1
+    def calc_precision(self):
+        min = str(client.get_symbol_info(symbol=self.symbol)['filters'][2]['minQty'])
+        print("Min: ", min)
+        min = min.replace('.', '')
+        precision = min.find('1')
+        print("Precision: ", precision)
+        return precision
 
     def adjust_price(self, price):
         if type(price) is str:
@@ -78,33 +69,12 @@ class Signal:
         else:
             return 0
 
-    def calc_precision(self):
-        min = str(client.get_symbol_info(symbol=self.symbol)['filters'][2]['minQty'])
-        print("Min: ", min)
-        min = min.replace('.', '')
-        precision = min.find('1')
-        print("Precision: ", precision)
-        return precision
-
-    def calc_quantity(self):
-        quan = []
-        x = [0.4, 0.3, 0.2, 0.095]
-        for a in range(4):
-            tmp = str(self.buy_quantity)  # XD
-            tmp = float(tmp)  # XD
-            tmp = tmp * x[a]
-            quan.append(float(round(tmp, self.precision)))
-
-        print(quan)
-
-        return quan
-
     def calc_stop_limit(self):
         stop_limit = ''
         if self.stop_price[-1] == '9':
             tmp = int(self.stop_price[-2])
             tmp = tmp + 1
-            tmp_list = list(stop_price)
+            tmp_list = list(self.stop_price)
             tmp_list[-2] = str(tmp)
             tmp_list[-1] = '0'
             stop_limit = stop_limit.join(tmp_list)
@@ -117,8 +87,26 @@ class Signal:
         print("Stop limit: ", stop_limit)
         return stop_limit
 
+    def calc_sell_quantity(self):
+        quan = []
+        x = [0.4, 0.3, 0.2, 0.095]
+        for a in range(4):
+            tmp = str(self.buy_quantity)  # XD
+            tmp = float(tmp)  # XD
+            tmp = tmp * x[a]
+            quan.append(float(round(tmp, self.precision)))
+
+        print("Sell quantity: ", quan)
+        quan_float = [float(item) for item in quan]
+        print("Suma quan: ",sum(quan_float))
+        if sum(quan_float) > float(self.buy_quantity):
+            print("Suma za duza!")
+            signal.ok = 0
+
+        return quan
+
     def make_order(self):
-        if int(self.buy_prices[0]) > int(self.avg_price) > int(self.buy_prices[1]):
+        if float(self.sell_prices[0]>self.price):
             print("CENA OK")
             print("Buy quantity: ", self.buy_quantity)
             order = client.order_market_buy(
@@ -127,25 +115,22 @@ class Signal:
             print(order['status'])
             if order['status'] == 'FILLED':
                 ocos = []
-                adjusted_sell_prices = []
-                for a in self.sell_prices:
-                    adjusted_sell_prices.append(self.adjust_price(a))
-                adjusted_stop_limit = self.adjust_price(self.stop_limit)
-                adjusted_stop_price = self.adjust_price(self.stop_price)
-                print(adjusted_sell_prices)
                 for a in range(4):
                     oco = client.order_oco_sell(
                         symbol=self.symbol,
                         quantity=self.sell_quantity[a],
-                        price=adjusted_sell_prices[a],
-                        stopPrice=adjusted_stop_price,
-                        stopLimitPrice=adjusted_stop_limit,
+                        price=self.sell_prices[a],
+                        stopPrice=self.stop_price,
+                        stopLimitPrice=self.stop_limit,
                         stopLimitTimeInForce='FOK')
                     ocos.append(oco)
-                print(ocos)
+                    print("Zlecenie sprzedazy nr: ", a+1, " wystawione.")
+                #print(ocos)
+            print("Zlecenia wystawione!")
+            return 1
         else:
             print("CENA NIE OK")
-
+            return 0
 
 if __name__ == "__main__":
     ws = websocket.WebSocket()
@@ -168,7 +153,7 @@ if __name__ == "__main__":
     send_json_request(ws, payload)
 
     while True:
-        author = ""
+        content = None
         try:
             event = receive_json_response(ws)
         except:
@@ -177,14 +162,18 @@ if __name__ == "__main__":
             send_json_request(ws, payload)
         try:
             content = event['d']['content']
-            channel_id = event['d']['channel_id']
             author = event['d']['author']['username']
         except:
             pass
-
-        if author == "Nickname" and channel_id == "000":
-            data = binance(content)
-            if data != 0:
-                signal = Signal(data[0], data[1], data[2], data[3])
-                signal.make_order()
+        if content is not None:
+            if author == "Nickname":
+                print(f'{author}: {content}')
+                data = binance(content)
+                print(data)
+                if len(data)==3:
+                    signal = Signal(data[0], data[1], data[2])
+                    if(signal.ok==1):
+                        signal.make_order()
+                    else:
+                        print("Blad, signal ok = 0")
 
